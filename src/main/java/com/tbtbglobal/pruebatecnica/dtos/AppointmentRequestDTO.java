72f2a875d9926898c940a9a4dc7819a5d8081df2
Ronald Rosero <ronaldes93@gmail.com>
2024-04-23 21:01:00 -0500

    Validations are added to the appointment dto

--- src/main/java/com/tbtbglobal/pruebatecnica/dtos/AppointmentRequestDTO.java
+++ src/main/java/com/tbtbglobal/pruebatecnica/dtos/AppointmentRequestDTO.java
@@ -1,6 +1,8 @@
 package com.tbtbglobal.pruebatecnica.dtos;
 
 
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.sql.Time;
@@ -16,12 +18,17 @@ public class AppointmentRequestDTO {
 
     private Integer appointmentId;
 
+    @NotNull(message = "La fecha de la cita no puede ser nula.")
+    @FutureOrPresent(message = "La fecha de la cita no debe estar en el pasado.")
     private LocalDate appointmentDate;
 
+    @NotNull(message = "La hora de la cita no puede ser nula.")
     private Time appointmentTime;
 
+    @NotNull(message = "El id del paciente no puede ser nulo.")
     private  String patientId;
 
+    @NotNull(message = "El id del doctor no puede ser nulo.")
     private  String doctorId;
 
     private boolean isAttended;
